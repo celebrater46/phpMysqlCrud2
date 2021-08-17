c05b83843f30cb1586aaa816331dc2aa222752ac
hideru <jota.16.stigma.world@gmail.com>
2021-08-17 23:53:05 +0900

    Renamed files

--- docker/php/test/index.php
+++ docker/php/test/index.php
@@ -1,22 +1,47 @@
 <?php
-try {
-//    https://qiita.com/fujitak/items/b56122e2ecd94022a7b6
-    # hostには「docker-compose.yml」で指定したコンテナ名を記載
-    $dsn = "mysql:host=mysql_host;dbname=test_database;";
-//    $db = new PDO($dsn, 'docker', 'docker');
-    $db = new PDO($dsn, 'docker', 'docker');
-//    $db = new PDO($dsn, 'root', 'root');
 
-//    $sql = "SELECT * FROM test_table";
-    // insert into [table-name] (column1, column2 … ) values(value1, value2 … );
-//    $sql = "insert into `test_table` (`name`, `age`, `hire_date`) values(`StoneSwamp`, `58`, `2018-06-28T00:00:00`)";
-    $sql = "insert into test_table (name, age, hire_date) values ('StoneSwamp', 58, '2018-06-28T00:00:00')";
-    $stmt = $db->prepare($sql);
-    $stmt->execute();
-    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    var_dump($result);
-    echo "Hello world 2302" . PHP_EOL;
-} catch (PDOException $e) {
-    echo $e->getMessage();
-    exit;
+// https://gray-code.com/php/insert-data-by-using-pdo/
+
+ini_set('display_errors', "On");
+define("DSN", "mysql:host=mysql_host;dbname=test_database;");
+
+function pdo($sql, $isPost, $data) {
+    try {
+        $pdo = new PDO(DSN, 'docker', 'docker');
+        $stmt = $pdo->prepare($sql);
+        if($isPost) {
+            $stmt->bindParam( ':name', $data["name"], PDO::PARAM_STR);
+            $stmt->bindParam( ':age', $data["age"], PDO::PARAM_STR);
+            $stmt->bindParam( ':hire', $data["hire"], PDO::PARAM_STR);
+        }
+        $stmt->execute();
+        $pdo = null; // DB接続解除
+        if(!$isPost) {
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            echo "POST succeeded!" . PHP_EOL;
+            var_dump($result);
+        }
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        exit;
+    }
 }
+
+function postData() {
+    $data = [
+        "name" => "Miyabi",
+        "age" => 23,
+        "hire" => "2018-06-28T00:00:00"
+    ];
+    $sql = "INSERT INTO test_table (name, age, hire_date) VALUES (:name, :age, :hire)";
+    pdo($sql, true, $data);
+}
+
+function getData() {
+    $sql = "SELECT * FROM test_table";
+    pdo($sql, false, null);
+}
+
+postData();
+getData();
+
