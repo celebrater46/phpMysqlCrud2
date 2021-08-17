200a30e23898388000f2d86c1edb447cd7a93c88
hideru <jota.16.stigma.world@gmail.com>
2021-08-18 01:57:32 +0900

    POST to var_dump.php succeeded

--- docker/php/test/var_dump.php
+++ docker/php/test/var_dump.php
@@ -3,7 +3,16 @@
 //var_dump($_POST);
 //var_dump($_SERVER);
 
-$name = $_POST["Name"];
-$age = $_POST["Age"];
-$hire = $_POST["HireDate"];
-echo $name . ", " . $age . ", " . $hire . PHP_EOL;
+//$name = $_POST["Name"];
+//$age = $_POST["Age"];
+//$hire = $_POST["HireDate"];
+//echo $name . ", " . $age . ", " . $hire . PHP_EOL;
+
+echo $_POST['Name'] . PHP_EOL;
+echo $_POST['Age'] . PHP_EOL;
+echo $_POST['HireDate'] . PHP_EOL;
+
+//$json = file_get_contents("php://input");
+//var_dump($json);
+
+?>
