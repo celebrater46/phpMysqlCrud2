dd32a21251c63f3c2f5928e8ad5f0f24bb29b1a3
hideru <jota.16.stigma.world@gmail.com>
2021-08-17 23:51:07 +0900

    Clean up index2.php

--- docker/php/test/index2.php
+++ docker/php/test/index2.php
@@ -3,8 +3,6 @@
 // https://gray-code.com/php/insert-data-by-using-pdo/
 
 ini_set('display_errors', "On");
-
-//$dsn = "mysql:host=mysql_host;dbname=test_database;";
 define("DSN", "mysql:host=mysql_host;dbname=test_database;");
 
 function pdo($sql, $isPost, $data) {
@@ -35,9 +33,6 @@ function postData() {
         "age" => 23,
         "hire" => "2018-06-28T00:00:00"
     ];
-//    $name = 'Miyon';
-//    $age = 28;
-//    $hire = '2018-06-28T00:00:00';
     $sql = "INSERT INTO test_table (name, age, hire_date) VALUES (:name, :age, :hire)";
     pdo($sql, true, $data);
 }
@@ -49,31 +44,4 @@ function getData() {
 
 postData();
 getData();
-
-//try {
-//    // POST
-//    $name = 'Miyon';
-//    $age = 28;
-//    $hire = '2018-06-28T00:00:00';
-//    $sql = "INSERT INTO test_table (name, age, hire_date) VALUES (:name, :age, :hire)";
-//    $pdo = new PDO(DSN, 'docker', 'docker');
-//    $stmt = $pdo->prepare($sql);
-//    $stmt->bindParam( ':name', $name, PDO::PARAM_STR);
-//    $stmt->bindParam( ':age', $age, PDO::PARAM_STR);
-//    $stmt->bindParam( ':hire', $hire, PDO::PARAM_STR);
-//    $res = $stmt->execute();
-//    // データベースの接続解除
-//    $pdo = null;
-//
-//    // GET
-//    $sql2 = "SELECT * FROM test_table";
-//    $pdo2 = new PDO(DSN, 'docker', 'docker');
-//    $stmt2 = $pdo2->prepare($sql2);
-//    $stmt2->execute();
-//    $res2 = $stmt2->fetchAll(PDO::FETCH_ASSOC);
-//    echo "POST succeeded!" . PHP_EOL;
-//    var_dump($res2);
-//} catch (PDOException $e) {
-//
-//}
 
